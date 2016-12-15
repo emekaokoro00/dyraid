b0102aaa5c3ee84e32322c17f95281c151886ef1
emekaokoro <emeka.okoro.00@gmail.com>
2016-12-15 16:01:27 -0600

    started using generic views

--- home/urls.py
+++ home/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<meal_type_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<meal_type_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # url(r'^(?P<pk>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<meal_type_id>[0-9]+)/increase_calorie_for_meal/$', views.increase_calorie_for_meal, name='increase_calorie_for_meal'),
     url(r'^(?P<logger_id>[0-9]+)/rate/$', views.rate, name='rate'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,25 +3,45 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
+from django.views import generic
 
 from .models import Meal_Type, Meal
 
 
-def index(request):
-    latest_meal_type_list = Meal_Type.objects.order_by('-meal_type_name')[:5]
-    context = {'latest_meal_type_list': latest_meal_type_list}
-    return render(request, 'home/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'home/index.html'
+    context_object_name = 'latest_meal_type_list'
 
-# Logger with rating, Meal with Meal Type
-def detail(request, meal_type_id):
-    meal_type = get_object_or_404(Meal_Type, pk=meal_type_id)
-    context = {'meal_type': meal_type}
-    return render(request, 'home/detail.html', context)
+    def get_queryset(self):
+        """Return the last five meal types."""
+        return Meal_Type.objects.order_by('meal_type_name')[:2]
 
-def results(request, meal_type_id):
-    meal_type = get_object_or_404(Meal_Type, pk=meal_type_id)
-    context = {'meal_type': meal_type}
-    return render(request, 'home/results.html', context)
+
+class DetailView(generic.DetailView):
+    model = Meal_Type
+    template_name = 'home/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Meal_Type
+    template_name = 'home/results.html'
+
+
+# def index(request):
+#     latest_meal_type_list = Meal_Type.objects.order_by('-meal_type_name')[:5]
+#     context = {'latest_meal_type_list': latest_meal_type_list}
+#     return render(request, 'home/index.html', context)
+# 
+# # Logger with rating, Meal with Meal Type
+# def detail(request, meal_type_id):
+#     meal_type = get_object_or_404(Meal_Type, pk=meal_type_id)
+#     context = {'meal_type': meal_type}
+#     return render(request, 'home/detail.html', context)
+# 
+# def results(request, meal_type_id):
+#     meal_type = get_object_or_404(Meal_Type, pk=meal_type_id)
+#     context = {'meal_type': meal_type}
+#     return render(request, 'home/results.html', context)
 
 def increase_calorie_for_meal(request, meal_type_id):
     meal_type = get_object_or_404(Meal_Type, pk=meal_type_id)
@@ -34,7 +54,7 @@ def increase_calorie_for_meal(request, meal_type_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_meal.calories += 2
+        selected_meal.calories += 1
         selected_meal.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
