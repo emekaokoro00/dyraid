c8ccb1feb3b062b41acac7852c3c281f0b961768
emekaokoro <emeka.okoro.00@gmail.com>
2016-12-29 21:09:05 -0600

    prog4

---
+++ home/static/home/overrides/build/js/custom.js
@@ -0,0 +1,21 @@
+/**
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var CURRENT_URL = window.location.href.split('?')[0],
+    $BODY = $('body'),
+    $MENU_TOGGLE = $('#menu_toggle'),
+    $SIDEBAR_MENU = $('#sidebar-menu'),
+    $SIDEBAR_FOOTER = $('.sidebar-footer'),
+    $LEFT_COL = $('.left_col'),
+    $RIGHT_COL = $('.right_col'),
+    $NAV_MENU = $('.nav_menu'),
+    $FOOTER = $('footer');
+
+// Sidebar
+$(document).ready(function() {
+    // trigger click
+    $MENU_TOGGLE.trigger('click');
+})(jQuery,'smartresize');
