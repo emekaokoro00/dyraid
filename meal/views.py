aef123fb4eee6c60451a927fc51b67a33b12af1e
emekaokoro <emeka.okoro.00@gmail.com>
2017-04-02 18:53:33 -0500

    num1

--- meal/views.py
+++ meal/views.py
@@ -42,3 +42,56 @@ class MealUpdate(LoginRequiredMixin, UpdateView):
 class MealDelete(LoginRequiredMixin, DeleteView):
     model = Meal
     success_url = reverse_lazy('meal:meal_list')
+    
+
+#FBV
+# def meal_list(request, template_name='home/meal_list.html'):
+#     meal_list = Meal.objects.all()
+# #     data = {}
+# #     data['object_list'] = meals
+# #     return render(request, template_name, data)
+#     context = {'meal_list':meal_list}
+#     return render(request, template_name, context)
+#     
+# def meal_create(request, template_name='home/meal_form.html'):
+#     form = MealForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('meal_list')
+#     return render(request, template_name, {'form':form})  
+# 
+# def meal_detail(request, pk, template_name='home/meal_detail.html'):
+#     meal = get_object_or_404(Meal, pk=pk)
+#     form = MealDetailForm(request.POST or None, instance=meal)
+#     context = {'meal':meal}
+#     return render(request, template_name, context)
+#     
+# def meal_update(request, pk, template_name='home/meal_form.html'):
+#     meal = get_object_or_404(Meal, pk=pk)
+#     form = MealForm(request.POST or None, instance=meal)
+#     if form.is_valid():
+#         form.save()
+#         return redirect('home:meal_list')
+#     return render(request, template_name, {'form':form})
+#     
+# def meal_delete(request, pk, template_name='home/meal_confirm_delete.html'):
+#     meal = get_object_or_404(Meal, pk=pk)
+#     form = MealForm(request.POST or None, instance=meal)
+#     if request.method=='POST':
+#         meal.delete()
+#         return redirect('meal_list')
+#     return render(request, template_name, {'object':meal})
+
+
+#CBV
+# class MealSendCaloriesView(FormView):
+#     template_name = 'home/generic_action.html'
+#     form_class = MealSendCaloriesForm
+#     success_url = 'home/thanks/'
+#     def form_valid(self, form):
+#         # This method is called when valid form data has been POSTed
+#         # It should retunr an HttpResponse
+#         form.send_calorie_number()
+#         return super(MealSendCaloriesView, self).form_valid(form)
+
+#LoginRequiredMixin to make sure that user is logged in, redirects to url in settings
